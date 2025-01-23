eb1beb5f956a0574db587468012082db8037b202
Will Cornwell <w.cornwell@unsw.edu.au>
2025-01-23 16:19:24 +1100

    more history

--- scripts/fig_records_through_time.R
+++ scripts/fig_records_through_time.R
@@ -24,10 +24,10 @@ both<-rbind(select(roy_vouch,year,park,`Record type`),select(yos_vouch,year,park
 
 # Event data frame
 events <- data.frame(
-  year = c(1890, 1864,1879),
-  event = c("Park Formation", "Yosemite Grant","Park Formation"),
-  park = c("Yosemite", "Yosemite","Royal"),
-  y = c(3200,3200, 1900)  # Adjust the vertical position as needed
+  year = c(1890, 1864,1879,1840,1854),
+  event = c("Park Formation", "Yosemite Grant","Park Formation","First Extant Collection","First Extant Collection"),
+  park = c("Yosemite", "Yosemite","Royal","Royal","Yosemite"),
+  y = c(3400,3400, 1950,2400,4300)  # Adjust the vertical position as needed
 )
 
 # Plot
